e224e151d41b27507d3aed41cce64f7b6397f6ca
Евгений <msk9199625687@yandex.ru>
2023-03-15 13:54:10 +0300

    lesson-3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import Categories from "./components/Categories/Categories";
+import Header from "./components/Header/Header";
+import PizzaItem from "./components/PizzaItem/PizzaItem";
+import Sort from "./components/Sort/Sort";
+import "./scss/app.scss";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaItem title="dasd" price="asda" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
