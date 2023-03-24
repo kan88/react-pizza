b603c3e5ac2210ea2bd91de4b0e367f172193e45
Евгений <msk9199625687@yandex.ru>
2023-03-24 22:01:21 +0300

    add pagination

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Categories from "../../components/Categories/Categories";
+import Pagination from "../../components/Pagination/Pagination";
 import PizzaItem from "../../components/PizzaItem/PizzaItem";
 import Skeleton from "../../components/Skeleton/Skeleton";
 import Sort from "../../components/Sort/Sort";
@@ -17,11 +18,12 @@ export function Home({ value }) {
   const sortFind = `sortBy=${sortName}&order=${sortValue}`;
   const filterFind = filter > 0 ? `category=${filter}&` : "";
   const searchValue = value ? `&search=${value}` : "";
+  const [activePage, setActivePage] = useState(1);
 
   const getPizzas = async () => {
     setLoading(true);
     const response = await fetch(
-      `https://64130d1db1ea744303210dcb.mockapi.io/api/pizzas?${filterFind}${sortFind}${searchValue}`
+      `https://64130d1db1ea744303210dcb.mockapi.io/api/pizzas?page=${activePage}&limit=4&${filterFind}${sortFind}${searchValue}`
     );
     const pizzas = await response.json();
     setPizzas(pizzas);
@@ -30,7 +32,7 @@ export function Home({ value }) {
   useEffect(() => {
     getPizzas();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filter, sort, value]);
+  }, [filter, sort, value, activePage]);
   return (
     <>
       <div className="content__top">
@@ -42,6 +44,9 @@ export function Home({ value }) {
         {isLoading
           ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
           : pizzas.map((pizza, index) => <PizzaItem key={index} {...pizza} />)}
+      </div>
+      <div>
+        <Pagination setActivePage={setActivePage} />
       </div>
     </>
   );
